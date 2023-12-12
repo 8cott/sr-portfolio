8da8b46b08d6c4c9e9b2939f6c33325be63b58d8
Scott <109820423+8cott@users.noreply.github.com>
2023-12-12 17:38:33 -0500

    added new resume

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -39,7 +39,7 @@ const Contact = () => {
         {/* Card in the back with a consistent aspect ratio and position */}
         <div className='absolute inset-0 bg-gradient-to-r from-cyan-700 to-teal-600 shadow-lg transform skew-y-0 rotate-0 rounded-3xl sm:skew-y-0 sm:rotate-6 sm:rounded-3xl'></div>
         {/* Card in the front with a consistent aspect ratio and position */}
-        <div className='relative px-4 py-10 bg-gradient-to-r from-teal-500 to-teal-400 shadow-lg rounded-3xl sm:rounded-3xl sm:p-20'>
+        <div className='relative px-4 py-10 bg-gradient-to-r from-teal-600 to-teal-500 shadow-lg rounded-3xl sm:rounded-3xl sm:p-20'>
           <div className='text-center pb-6'>
             <h1 className='text-3xl text-white'>Contact Scott</h1>
             <p className='text-gray-200'>
